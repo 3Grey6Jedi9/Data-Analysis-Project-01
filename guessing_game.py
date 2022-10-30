e52e4f0a9eb6ae9bc0c125a1bdd31404b2d79b2e
Darth Vader <danieltarancon@gmail.com>
2022-10-30 17:27:18 +0100

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -6,15 +6,20 @@ players_name = []
 
 players_score = {}
 
+
+
+
+
 def start_game():
     print('''\n\t*** WELCOME LADIES AND GENTLEMEN TO THE GUESSING NUMBER GAME ***\n''')
     ranking()
     magic_number = random.randrange(1,101)
     number_guesses = 0
+    again = 'y'
     name = input('Would you be so kind of telling me your name please? ')
     players_name.append(name)
-    print(f'Alright {name}!! I hope you are ready... Let is beging!')
-    while ValueError or magic_number != guess:
+    print(f'Alright {name}!! I hope you are ready... Let us beging!')
+    while (ValueError or magic_number != guess) and again == 'y':
         try:
             guess = int(input('Please enter an integer between 1 and 100: '))
             if guess not in range(1,101):
@@ -33,6 +38,22 @@ def start_game():
             else:
                 print('Well done you guessed the right number!!!')
                 players_score[name] = number_guesses
+                data(number_guesses, players_score)
+                print('\nAfter your performance the current ranking looks like this:\n')
+                ranking()
+                while ValueError:
+                    try:
+                        again = input('\nWould you like to play again[y/n]? ')
+                        if again != 'y' and again != 'n':
+                            raise ValueError("Please enter 'y' for 'yes' or 'n' for 'no' ")
+                    except ValueError as err:
+                        print(f'{err}')
+                    else:
+                        if again == 'n':
+                            print('\nVery well then... THE GAME IS OVER')
+                            break
+                        else:
+                            break
 
 
 
@@ -43,6 +64,18 @@ def start_game():
 
 
 
+
+
+
+def data(number_guesses, players_score):
+    results = []
+    for value in players_score.values():
+        results.append(value)
+    print(f'''Here are some statistics:
+     \rNumber of attempts: {number_guesses}
+     \rMean: {statistics.mean(results)}
+     \rMedian: {statistics.median(results)}
+     \rMode: {statistics.mode(results)}''')
 
 
 
